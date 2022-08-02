31b438141b0eeed24bed482149c9aedd6e282332
adikrs <44467907+adikrs@users.noreply.github.com>
2022-08-02 17:03:41 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] s) {
         System.out.println("Hello Oracle");
         for(int i = 0; i<=10; i++) {
-            System.out.println("Value of I : " + i);
+            System.out.println("Value of I updated: " + i);
         }
     }
 }
